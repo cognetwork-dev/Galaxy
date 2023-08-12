ff01f9b40ccb39f999d7bf25e861a88224a1bc5d
Nebelung <nebelung@mailfence.com>
2023-08-12 23:18:56 +0000

    Done for today

---
+++ src/root.tsx
@@ -0,0 +1,39 @@
+// @refresh reload
+import { Suspense } from "solid-js";
+import {
+  A,
+  Body,
+  ErrorBoundary,
+  FileRoutes,
+  Head,
+  Html,
+  Meta,
+  Routes,
+  Scripts,
+  Title,
+} from "solid-start";
+import "./root.css";
+
+export default function Root() {
+  return (
+    <Html lang="en">
+      <Head>
+        <Title>Nebula</Title>
+        <Meta charset="utf-8" />
+        <Meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Body>
+        <Suspense>
+          <ErrorBoundary>
+            <A href="/">Home</A>
+            <A href="/options">Options</A>
+            <Routes>
+              <FileRoutes />
+            </Routes>
+          </ErrorBoundary>
+        </Suspense>
+        <Scripts />
+      </Body>
+    </Html>
+  );
+}
---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { Title } from "solid-start";
+/*import Counter from "~/components/Counter";*/
+
+export default function Home() {
+  return (
+    <main>
+      <Title>Nebula</Title>
+      <h1>Hello world!</h1>
+    </main>
+  );
+}
