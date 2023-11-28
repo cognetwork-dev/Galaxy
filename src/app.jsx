e6984d209fd5fa4269d6ebd4aef44b272c7c54e3
mavy16 <145229977+mavy16@users.noreply.github.com>
2023-11-27 19:20:22 -0500

    s

--- src/app.jsx
+++ src/app.jsx
@@ -15,7 +15,7 @@ function App() {
     <>
       <Head />
       <div class="home">
-        <div class="title">Mavs Prox</div>
+        <div class="title">Mavs 2.0</div>
         <div class="omnibox">
           <div class="searchIcon">
             <IoSearch fill="var(--text-inverse)" />
